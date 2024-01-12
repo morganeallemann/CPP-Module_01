861a46c0a39300749dbc5813cb80162844ed6f2c
malleman <malleman@student.42lausanne.ch>
2024-01-12 18:09:09 +0100

    in process

---
+++ ex05/Harl.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malleman <malleman@42lausanne.ch>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 15:37:09 by malleman          #+#    #+#             */
+/*   Updated: 2024/01/11 16:11:18 by malleman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+
+}
+
+Harl::~Harl()
+{
+
+}
+
+void    Harl::debug(void)
+{
+    std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void    Harl::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void    Harl::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void    Harl::error(void)
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void    Harl::complain(std::string level)
+{
+
+}
---
+++ ex05/Harl.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malleman <malleman@42lausanne.ch>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 15:37:09 by malleman          #+#    #+#             */
+/*   Updated: 2024/01/11 16:11:18 by malleman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_H
+# define HARLE_H
+
+#include<iostream>
+#include<string>
+
+class Harl
+{
+private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+
+public:
+    Harl();
+    ~Harl();
+    void complain(std::string level);
+};
+
+
+#endif
